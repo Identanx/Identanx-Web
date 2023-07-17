3454a6e806df0f0d79b80cab4cb03e4aa9d2285b
LeoYeudiel <lyeuprsw@gmail.com>
2023-07-17 17:45:35 -0600

    add functions and design about menu-hamburguer

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 5000,
+  }, 
+
+});
+
+const menu = document.getElementById('menu-hamburguer');
+const nav = document.getElementById('navegation-burger');
+  
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('closed') || nav.classList.contains('closed')) {
+    menu.classList.remove('closed');
+    nav.classList.remove('closed');
+
+    menu.classList.add('opened');
+    nav.classList.add('opened');
+  } else {
+    menu.classList.remove('opened');
+    nav.classList.remove('opened');
+
+    menu.classList.add('closed');
+    nav.classList.add('closed');
+  }
+});
+
+
+
+
+
